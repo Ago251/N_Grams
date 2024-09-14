6d138ecb4f96ccc565492c9fb7269d81746e42b1
Andrea Agostini <andreaagostini1998@gmail.com>
2024-09-14 09:03:29 +0200

    Created UIPlayerDataExtensions class

---
+++ Learning/Assets/UIPlayerDataExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class UIPlayerDataExtensions
+{
+    public static Sprite GetSign(this UIPlayerData data, SignType sign)
+    {
+        switch (sign)
+        {
+            case SignType.Rock:
+                return data.Rock;
+            case SignType.Paper:
+                return data.Paper;
+            case SignType.Scissor:
+                return data.Scissor;
+            default:
+                return null;
+        }
+    }
+}
